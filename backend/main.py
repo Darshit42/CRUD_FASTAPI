eb437639ba92b4dad26d913950a95a58a7a96305
Darshit <shadow6920223@gmail.com>
2025-12-15 15:01:04 +0530

    import error

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,10 @@
+import sys
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent
+if str(ROOT_DIR) not in sys.path:
+    sys.path.append(str(ROOT_DIR))
+
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
